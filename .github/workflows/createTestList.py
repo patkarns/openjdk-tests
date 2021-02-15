3bf618a455229591ca30fcdef3703ab1da04250e
patkarns <30379111+patkarns@users.noreply.github.com>
2021-02-15 04:16:20 -0800

    Update createTestList.py

--- .github/workflows/createTestList.py
+++ .github/workflows/createTestList.py
@@ -3,25 +3,19 @@ import os
 import ast
 
 def main():
-    print(sys.argv)
-    print(os.getenv('TARGET_LIST'))
     
     sanity_format = "sanity.{}"
     formattedTests = []
     
     targetNames = ast.literal_eval(os.getenv('TARGET_LIST'))
     
-    print(targetNames)
-    
     for argument in targetNames:
-        print(argument)
         formattedTests.append(sanity_format.format(argument))
     
     allTests = ','.join(formattedTests)    
     testTargets = 'TESTLIST={}'.format(allTests)
     
     print(testTargets)
-    
     print('::set-output name=test_targets_str::{}'.format(testTargets))
 
 if __name__ == "__main__":

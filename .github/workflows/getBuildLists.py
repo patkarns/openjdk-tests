39047bd757869e467049be931762e78dfc4d0ec9
YananW <40300331+xfxcwynlc@users.noreply.github.com>
2021-02-15 22:41:20 -0500

    Update getBuildLists.py
    To set build_env parameter equals true when detects changes in 'buildenv/'

--- .github/workflows/getBuildLists.py
+++ .github/workflows/getBuildLists.py
@@ -6,6 +6,7 @@ def main():
     print(sys.argv)
     result = []
     # TODO: handle the change of buildenv dir
+    flag = False
     for argument in sys.argv:
         if (argument.startswith('perf/') and ('perf' not in result)):
             result.append('perf')
@@ -15,6 +16,9 @@ def main():
             result.append('functional')
         elif (argument.startswith('openjdk/') and ('openjdk' not in result)):
             result.append('openjdk')
+        if argument.startswith('buildenv/') and not flag:
+            print('::set-output name=build_env::true')
+            flag = True
 
     if not result:
         result.append('skip')
